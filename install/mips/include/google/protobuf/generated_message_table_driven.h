900af4f5953ebfdaefe7fac957a41f40f55328d7
Artem Bykov <artem.bykov@prism.com>
2018-04-06 17:36:40 +0300

    removed PROTOBUF_CONSTEXPR

--- install/mips/include/google/protobuf/generated_message_table_driven.h
+++ install/mips/include/google/protobuf/generated_message_table_driven.h
@@ -38,20 +38,6 @@
 #include <google/protobuf/wire_format_lite.h>
 #include <google/protobuf/wire_format_lite_inl.h>
 
-#if LANG_CXX11
-#define PROTOBUF_CONSTEXPR constexpr
-
-// We require C++11 and Clang to use constexpr for variables, as GCC 4.8
-// requires constexpr to be consistent between declarations of variables
-// unnecessarily (see https://gcc.gnu.org/bugzilla/show_bug.cgi?id=58541).
-// VS 2017 Update 3 also supports this usage of constexpr.
-#if defined(__clang__) || (defined(_MSC_VER) && _MSC_VER >= 1911)
-#define PROTOBUF_CONSTEXPR_VAR constexpr
-#else  // !__clang__
-#define PROTOBUF_CONSTEXPR_VAR
-#endif  // !_clang
-
-#else
 #define PROTOBUF_CONSTEXPR
 #define PROTOBUF_CONSTEXPR_VAR
 #endif
